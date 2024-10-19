d2df0ab2da0247e7c1edd3f20a2288a08fcba070
Jennifer Verty 2327250040 <jenniferverty0605@gmail.com>
2024-10-19 13:03:42 +0700

    Candiselesai

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/detail_screen.dart';
-import 'package:wisata_candi/models/candi.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DetailScreen(
-        candi: candiList[8] ,),
-    );
-  }
+        candi: candiList[0],
+     ),
+);
+}
 }
